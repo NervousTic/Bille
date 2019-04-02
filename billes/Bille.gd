ec84b7f29b6f77bf6c2df88c9cc57c609da792c3
NervousTic <nervous.tic.studio@gmail.com>
2019-04-02 17:24:58 +0200

    Redirection du signal de la bille

---
+++ billes/Bille.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Bille_body_entered(body):
+	if body.is_in_group("rebond"):
+		linear_velocity *= 5
+	pass # Replace with function body.
--- niveaux/Niveau.gd
+++ niveaux/Niveau.gd
@@ -8,14 +8,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-
-func _on_Bille_body_entered(body):
-	if body.is_in_group("rebond"):
-#		print ("position de la bille " + str($Bille.position))
-#		$Bille.apply_impulse(Vector2(0, 0), $Bille.linear_velocity * 20)
-		$Bille.linear_velocity *= 5
-
-
 func _on_Timer_timeout():
 	var bille = sac_de_bille.instance()
 	bille.position = position_initiale
